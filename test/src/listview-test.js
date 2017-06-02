7be8be3c3a8c1d8b857019339129e7a6f82730c1
Tetsuya Mori <t2y3141592@gmail.com>
2017-06-02 18:25:51 +0900

    [hybrid][test] item selection test for listitem

--- test/src/listview-test.js
+++ test/src/listview-test.js
@@ -8,6 +8,48 @@ Copyright (c) 2017, Tetsuya Mori <t2y3141592@gmail.com>. All rights reserved.
   let listview = new Suite(scope, 'live-localizer listview tests');
   listview.htmlSuite = 'live-localizer';
   listview.test = Suite.scopes.panel.classes.SelectListView;
+  listview.test = (base) => class ListViewItemsTest extends base {
+    async operation() {
+      let self = this;
+      self.list = self.listView.$.list;
+      await self.checkInterval(() => self.list.items.length === 6, 100 /* ms */, 20 /* times */);
+    }
+    async checkpoint() {
+      assert.equal(this.list.items.length, 6, 'list item length is 6');
+    }
+  }
+  listview.test = (base) => class ListViewSelectItemTest extends base {
+    * iteration() {
+      for (let i = 5; i >= 0; i--) {
+        yield { i: i, name: 'listItems[' + i + '] is selected' }
+      }
+    }
+    async operation(parameters) {
+      let self = this;
+      self.list = self.listView.$.list;
+      await self.forEvent(self.model, 'html-lang-mutation',
+        () => {
+          if (self.listView.version1) {
+            self.list.selection.select(parameters.i);
+          }
+          if (self.listView.version2) {
+            self.list.selectedItems = [self.listView.listItems[parameters.i]];
+          }
+        },
+        (element, type, event) => true);
+    }
+    async checkpoint(parameters) {
+      if (this.listView.version1) {
+        assert.equal(this.list.selection.selected().length, 1, 'Only 1 item is selected');
+        assert.equal(this.list.selection.selected()[0], parameters.i, 'Selected item is ' + this.listView.listItems[parameters.i][0]);
+      }
+      if (this.listView.version2) {
+        assert.equal(this.list.selectedItems.length, 1, 'Only 1 item is selected');
+        assert.equal(this.list.selectedItems[0][0], this.listView.listItems[parameters.i][0], 'Selected item is ' + this.listView.listItems[parameters.i][0]);
+      }
+      assert.equal(this.model.html.lang, this.listView.listItems[parameters.i][1], 'Selected locale is ' + this.listView.listItems[parameters.i][1]);
+    }
+  }
   /*
   iconview.test = (base) => class DropareaTooltipTest extends base {
     async operation() {
@@ -509,6 +551,10 @@ Copyright (c) 2017, Tetsuya Mori <t2y3141592@gmail.com>. All rights reserved.
     // test class mixins
     '': [],
     // test classes
-    SelectListView: 'SelectListViewTest'
+    SelectListView: {
+      ListViewItemsTest: {
+        ListViewSelectItemTest: 'ListViewSelectItemTest; Select each item in listview'
+      }
+    }
   };
 } // panel scope
