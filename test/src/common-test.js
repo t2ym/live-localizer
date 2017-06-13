751d012fa60c3e33cee9e70749d67df1642e59d7
Tetsuya Mori <t2y3141592@gmail.com>
2017-06-13 22:53:09 +0900

    [hybrid][test] Remove ineffective mocha.setup() for WCT

--- test/src/common-test.js
+++ test/src/common-test.js
@@ -2,7 +2,6 @@
 @license https://github.com/t2ym/live-localizer/blob/master/LICENSE.md
 Copyright (c) 2016, Tetsuya Mori <t2y3141592@gmail.com>. All rights reserved.
 */
-mocha.setup({ slow: 8000, timeout: 15000 });
 // global test classes
 class LiveLocalizerSuite extends Suite {
   static get reconnectable() { return false; }
