ea443f85eea5a76d28dff96afec4be941754e85c
Tetsuya Mori <t2y3141592@gmail.com>
2017-06-23 14:32:28 +0900

    [test] Longer timeout for FirebaseStorageInit

--- test/src/firebasestorage-test.js
+++ test/src/firebasestorage-test.js
@@ -368,7 +368,7 @@ Copyright (c) 2017, Tetsuya Mori <t2y3141592@gmail.com>. All rights reserved.
     async operation() {
       if (this.hasToSkip) { return; }
       let self = this;
-      await self.checkInterval(() => self.firebaseStorage.isSettingsInitialized, 200, 300);
+      await self.checkInterval(() => self.firebaseStorage.isSettingsInitialized, 200, 1500);
     }
     async checkpoint() {
       if (this.hasToSkip) { return; }
